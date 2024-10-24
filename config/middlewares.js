ebe5e49950bad4c9099109021506fb41c2e4867b
tarielakmatov <tariel.akmatiov@gmail.com>
2024-10-24 21:20:26 +0200

    www

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,17 +9,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  async (ctx, next) => {
-    // Custom inline middleware logic for specific routes
-    if (ctx.request.path.startsWith('/votes/findByReport')) {
-      // Example: Check if the user is authenticated
-      if (!ctx.state.user) {
-        // If not authenticated, return unauthorized response
-        return ctx.unauthorized('You must be logged in to access this resource');
-      }
-      // You can add more logic here as needed
-    }
-    // Continue to the next middleware/controller if the user is authenticated or it's not the specific route
-    await next();
-  },
 ];
